4e5e5f93381448ed5a3cbb4c78df75f2cf0ac152
GuiSebastiao <guilhermesebastiaou.u@gmail.com>
2025-05-22 15:03:55 -0300

    arquivos iniciais do projeto

---
+++ src/test/java/com/minerva/minervaapi/MinervaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minerva.minervaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
